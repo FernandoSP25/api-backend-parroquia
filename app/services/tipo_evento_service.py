7258b98709b5998ae89574544c0d1810642ecfa3
FernandoSP25 <fsolanoporto@gmail.com>
2026-04-11 10:14:24 -0500

    Actualizacion de eventos

--- app/services/tipo_evento_service.py
+++ app/services/tipo_evento_service.py
@@ -2,42 +2,42 @@ from sqlalchemy.orm import Session
 from app.models.tipo_evento import TipoEvento
 from app.schemas.tipo_evento import TipoEventoCreate, TipoEventoUpdate
 
+class TipoEventoService:
+    @staticmethod
+    def crear(db: Session, data: TipoEventoCreate):
+        obj = TipoEvento(**data.model_dump())
+        db.add(obj)
+        db.commit()
+        db.refresh(obj)
+        return obj
 
-def crear(db: Session, data: TipoEventoCreate):
-    obj = TipoEvento(**data.model_dump())
-    db.add(obj)
-    db.commit()
-    db.refresh(obj)
-    return obj
+    @staticmethod
+    def listar(db: Session):
+        return (
+            db.query(TipoEvento)
+            .filter(TipoEvento.activo == True)
+            .order_by(TipoEvento.orden)
+            .all()
+        )
 
+    @staticmethod
+    def obtener(db: Session, tipo_evento_id: int):
+        return db.query(TipoEvento).filter(TipoEvento.id == tipo_evento_id, TipoEvento.activo == True).first()
 
-def listar(db: Session):
-    return (
-        db.query(TipoEvento)
-        .filter(TipoEvento.activo == True)
-        .order_by(TipoEvento.orden)
-        .all()
-    )
+    @staticmethod
+    def actualizar(db: Session, tipo_evento_id: int, data: TipoEventoUpdate):
+        obj = TipoEventoService.obtener(db, tipo_evento_id)
+        if obj:
+            for field, value in data.model_dump(exclude_unset=True).items():
+                setattr(obj, field, value)
+            db.commit()
+            db.refresh(obj)
+        return obj
 
-
-def obtener(db: Session, tipo_evento_id: int):
-    return (
-        db.query(TipoEvento)
-        .filter(TipoEvento.id == tipo_evento_id, TipoEvento.activo == True)
-        .first()
-    )
-
-
-def actualizar(db: Session, obj: TipoEvento, data: TipoEventoUpdate):
-    for field, value in data.model_dump(exclude_unset=True).items():
-        setattr(obj, field, value)
-
-    db.commit()
-    db.refresh(obj)
-    return obj
-
-
-def eliminar(db: Session, obj: TipoEvento):
-    obj.activo = False
-    db.commit()
-    return obj
+    @staticmethod
+    def eliminar(db: Session, tipo_evento_id: int):
+        obj = TipoEventoService.obtener(db, tipo_evento_id)
+        if obj:
+            obj.activo = False
+            db.commit()
+        return obj
