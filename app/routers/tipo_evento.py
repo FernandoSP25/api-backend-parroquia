37a26bcab61b59df21fb85204bcf4b491ffa312f
FernandoSP25 <fsolanoporto@gmail.com>
2026-04-12 04:52:53 -0500

    Correccion de error service

--- app/routers/tipo_evento.py
+++ app/routers/tipo_evento.py
@@ -7,24 +7,25 @@ from app.schemas.tipo_evento import (
     TipoEventoUpdate,
     TipoEventoOut,
 )
-from app.services import tipo_evento_service as service
+
+from app.services.tipo_evento_service import TipoEventoService
 
 router = APIRouter(prefix="/tipos-evento", tags=["Tipos de Evento"])
 
 
 @router.post("/", response_model=TipoEventoOut, status_code=status.HTTP_201_CREATED)
 def crear_tipo_evento(data: TipoEventoCreate, db: Session = Depends(get_db)):
-    return service.crear(db, data)
+    return TipoEventoService.crear(db, data)
 
 
 @router.get("/", response_model=list[TipoEventoOut])
 def listar_tipos_evento(db: Session = Depends(get_db)):
-    return service.listar(db)
+    return TipoEventoService.listar(db)
 
 
 @router.get("/{tipo_evento_id}", response_model=TipoEventoOut)
 def obtener_tipo_evento(tipo_evento_id: int, db: Session = Depends(get_db)):
-    obj = service.obtener(db, tipo_evento_id)
+    obj = TipoEventoService.obtener(db, tipo_evento_id)
     if not obj:
         raise HTTPException(status_code=404, detail="Tipo de evento no encontrado")
     return obj
@@ -36,17 +37,17 @@ def actualizar_tipo_evento(
     data: TipoEventoUpdate,
     db: Session = Depends(get_db),
 ):
-    obj = service.obtener(db, tipo_evento_id)
+    obj = TipoEventoService.obtener(db, tipo_evento_id)
     if not obj:
         raise HTTPException(status_code=404, detail="Tipo de evento no encontrado")
 
-    return service.actualizar(db, obj, data)
+    return TipoEventoService.actualizar(db, obj, data)
 
 
 @router.delete("/{tipo_evento_id}", response_model=TipoEventoOut)
 def eliminar_tipo_evento(tipo_evento_id: int, db: Session = Depends(get_db)):
-    obj = service.obtener(db, tipo_evento_id)
+    obj = TipoEventoService.obtener(db, tipo_evento_id)
     if not obj:
         raise HTTPException(status_code=404, detail="Tipo de evento no encontrado")
 
-    return service.eliminar(db, obj)
+    return TipoEventoService.eliminar(db, obj)
