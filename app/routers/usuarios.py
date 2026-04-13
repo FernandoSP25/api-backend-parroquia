2179ad36505ab7c13bb28be002a0dd130922cc97
FernandoSP25 <fsolanoporto@gmail.com>
2026-04-13 00:01:48 -0500

    actualizacion de perfil

--- app/routers/usuarios.py
+++ app/routers/usuarios.py
@@ -6,13 +6,40 @@ from uuid import UUID
 from app.dependencies.database import get_db
 from app.dependencies.auth import get_current_active_user, RoleChecker
 from app.schemas.usuario import UsuarioCreate, UsuarioUpdate, UsuarioOut
-# 1. CAMBIO IMPORTANTE: Importamos la CLASE
 from app.services.usuario_service import UsuarioService 
 
 router = APIRouter(prefix="/usuarios", tags=["Usuarios"])
 
 # Permisos
 allow_admin = RoleChecker(["ADMIN"])
+
+@router.get("/me", response_model=UsuarioOut)
+def obtener_mi_perfil(
+    current_user = Depends(get_current_active_user)
+):
+    """Obtiene los datos del usuario que inició sesión"""
+    return current_user
+
+@router.put("/me", response_model=UsuarioOut)
+def actualizar_mi_perfil(
+    usuario_update: UsuarioUpdate, 
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_active_user)
+):
+    """El usuario actualiza sus propios datos (Nombres, teléfono, etc)"""
+    return UsuarioService.update(db, current_user, usuario_update)
+
+@router.put("/me/password", response_model=UsuarioOut)
+def cambiar_mi_password(
+    usuario_update: UsuarioUpdate, 
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_active_user)
+):
+    """Endpoint dedicado a cambiar la contraseña del usuario actual."""
+    if not usuario_update.password:
+        raise HTTPException(status_code=400, detail="La nueva contraseña es requerida")
+    
+    return UsuarioService.update(db, current_user, usuario_update)
 
 @router.get("/", response_model=List[UsuarioOut])
 def listar_usuarios(
@@ -67,7 +94,6 @@ def desactivar_usuario(
     # Aquí podrías pasar el ID del admin que ejecuta la acción si tienes autenticación
     return UsuarioService.desactivar(db, user_id)
 
-# ✅ ENDPOINT 2: REACTIVAR
 @router.post("/{user_id}/reactivar", status_code=status.HTTP_200_OK)
 def reactivar_usuario(
     user_id: UUID, 
